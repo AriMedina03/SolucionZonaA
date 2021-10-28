b5e9f31e14f98bea7c90af09073e3b0267c064e4
IvanRomero03 <A00833623@itesm.mx>
2021-10-27 22:03:27 -0700

    Hola

--- ZonaA.ino
+++ ZonaA.ino
@@ -1,1 +1,21 @@
+#include <Roborregos/DireccionesCambiadasYair2.ino>
 
+void SolucionZonaA()
+{
+    puenteH.AvanzarCasilla();
+    bool CasillasVisitadas[3][4] = {
+        {false, false, false, false},
+        {false, false, false, false},
+        {false, false, false, false}};
+    bool SePuedeVisitar[5][7] = {
+        {false, false, false, false, false, false, false},
+        {false, true, true, true, true, true, false},
+        {false, true, true, true, true, true, false},
+        {false, true, true, true, true, true, false},
+        {false, false, false, false, false, false, false}};
+    String Colores[3][4] = {
+        {"", "", "", ""},
+        {"", "", "", ""},
+        {"", "", "", ""}};
+    int Posicion[2] = {2, 3};
+}
